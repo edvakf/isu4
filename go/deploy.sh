bfe79a105a56509426d0e9487b86a4958fcc069c
Keiji, Yoshimi <walf443@gmail.com>
2014-11-08 12:00:34 +0900

    added deploy.sh

---
+++ go/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e -x
+
+SERVERS=${SERVERS:-isucon1 isucon2 isucon3}
+
+# curl --data "deploy by $USER to $SERVERS" 'https://teamfreesozai.slack.com/services/hooks/slackbot?token=oxjd47qGfo59VhemVz43FQZF&channel=%23general'
+
+for SERVER in ${SERVERS}
+do
+    echo $SERVER
+    rsync --dry-run -avz ./ isucon@$SERVER:/home/isucon/webapp/go/
+    ssh -t isucon@$SERVER bash -c 'env; cd /home/isucon/webapp/go && /home/isucon/webapp/go/build.sh'
+done
+
